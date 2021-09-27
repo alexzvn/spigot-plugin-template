1ce006655ce7f77dd7096f4750feae48957108c0
Alex <41188285+alexzvn@users.noreply.github.com>
2021-09-27 14:31:45 +0000

    🎁

---
+++ src/main/java/dev/alexzvn/plugin/Plugin.java
@@ -0,0 +1,16 @@
+package dev.alexzvn.plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Plugin extends JavaPlugin {
+    
+    @Override
+    public void onEnable() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void onDisable() {
+        // TODO Auto-generated method stub
+    }
+}
